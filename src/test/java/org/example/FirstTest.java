cac45cc6ca1fc12454a9d86469bb20b4f1e6885d
ndammes <ndammes@protonmail.com>
2024-03-22 22:40:08 +0100

    Enabled headless

--- src/test/java/org/example/FirstTest.java
+++ src/test/java/org/example/FirstTest.java
@@ -4,13 +4,16 @@ import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
 
 public class FirstTest {
     WebDriver driver;
 
     @Test
     public void testGoogle(){
-        driver = new ChromeDriver();
+        ChromeOptions options = new ChromeOptions();
+        options.addArguments("--headless=new");
+        driver = new ChromeDriver(options);
         driver.get("https://www.google.de");
         String title = driver.getTitle();
         Assertions.assertEquals("Google", title);
