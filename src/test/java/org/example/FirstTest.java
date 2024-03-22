5daaea44cbc26e29fc01ff8e37cc77e3da100465
ndammes <ndammes@protonmail.com>
2024-03-22 22:36:29 +0100

    Deleted target

--- src/test/java/org/example/FirstTest.java
+++ src/test/java/org/example/FirstTest.java
@@ -1,24 +1,20 @@
-package testcases;
+package org.example;
 
+import org.junit.Test;
 import org.junit.jupiter.api.Assertions;
-import org.junit.jupiter.api.Test;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
 
-public class firsttestcase {
+public class FirstTest {
     WebDriver driver;
-    public void setup(){
-        driver = new ChromeDriver();
-    }
 
     @Test
-    public void firstTest(){
-        setup();
+    public void testGoogle(){
+        driver = new ChromeDriver();
         driver.get("https://www.google.de");
         String title = driver.getTitle();
         Assertions.assertEquals("Google", title);
         driver.close();
-
     }
 
 }
